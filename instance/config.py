d11ddf4e7d9bc9577c2df5d92869025ae5aa612c
mccafee025 <ademaamanka@gmail.com>
2022-05-18 15:22:14 +0300

    omit password

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,3 @@
 DATABASE_URL = 'postgresql+psycopg2://esagavin:moringa@localhost/blog'
 SECRET_KEY = 'choomba'
 MAIL_USERNAME = 'abdirahman.mumin@student.moringaschool.com'
-MAIL_PASSWORD = '*t%Xprjnw4zk5gff'
