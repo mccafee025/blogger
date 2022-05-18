a96dc85bc1b660bf811fcda4992000b920da54b6
mccafee025 <ademaamanka@gmail.com>
2022-05-18 15:04:26 +0300

    more configs

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,4 @@
 DATABASE_URL = 'postgresql+psycopg2://esagavin:moringa@localhost/blog'
 SECRET_KEY = 'choomba'
+MAIL_USERNAME = 'abdirahman.mumin@student.moringaschool.com'
+MAIL_PASSWORD = '*t%Xprjnw4zk5gff'
