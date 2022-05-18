b31bded24b13ae60932ed9ce3bb3625be9896aca
mccafee025 <ademaamanka@gmail.com>
2022-05-18 10:00:35 +0300

    add routes for authentication

---
+++ app/auth/views.py
@@ -0,0 +1,40 @@
+from flask import render_template,redirect,url_for,flash,request
+from flask_login import login_user,logout_user,login_required
+from ..models import User
+from .. import db
+from ..email import mail_message
+from . import auth
+from .forms import LoginForm,RegistrationForm
+
+@auth.route('/login', methods = ["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next')or url_for('main.index'))
+
+        flash('Invalid Username or Password')
+
+    title = 'Blog App | Login'
+    return render_template('auth/login.html', login_form = login_form, title = title)
+
+@auth.route('/register', methods = ["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth/login.html'))
+
+    title = 'New Account || Dev Blog'
+    return render_template('auth/register.html', form = form, title = title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for('main.index'))
